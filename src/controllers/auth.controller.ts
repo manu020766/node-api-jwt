3b92dec9866d0ed76eabd6ccb8d2523e931b1f4e
Manu <manu020766@yahoo.es>
2020-06-04 17:43:41 +0200

    add encrypt and validate password

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,8 +1,27 @@
 import { Request, Response } from 'express'
+import User, { IUser } from '../models/user'
+import jwt from 'jsonwebtoken' 
 
-export const signup = (req:Request, res:Response) =>  {
-    res.status(200).json({ message: 'singup'})
+export const signup = async (req:Request, res:Response) =>  {
+    const { username, email, password } = req.body
+
+    const user:IUser = new User({
+        username,
+        email,
+        password
+    })
+    user.password = await user.encryptPassword(user.password)
+    const newUser = await user.save()
+
+    const token = jwt.sign({ _id: newUser._id }, process.env.TOKEN_SECRET || 'token secret key')
+
+    res.header('auth-token', token).json(newUser)
+    // res.status(200).json({
+    //     newUser,
+    //     token
+    // })
 }
+
 export const signin = (req:Request, res:Response) =>  {
     res.status(200).json({ message: 'signin'})
 }
--- src/database.ts
+++ src/database.ts
@@ -2,7 +2,8 @@ import mongoose from 'mongoose'
 
 mongoose.connect('mongodb://localhost/test', {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex: true
 })
     .then(() => console.log('Database is connected'))
     .catch( err => console.log('Database is not connected error: ', err))
